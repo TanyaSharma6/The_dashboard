bd91817a01c820783e0d159ee29663951b30e0ed
Prashantbag <prashant.bag14@gmail.com>
2023-10-19 08:15:59 +0530

    created new dashboard

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,8 +8,9 @@ import {
     FaShoppingBag,
     FaThList
 }from "react-icons/fa";
-import { NavLink } from 'react-router-dom';
 
+import { NavLink } from 'react-router-dom';
+import dashboardImg from '../images/silver-settings-icon-free-png.webp'
 
 const Sidebar = ({children}) => {
     const[isOpen ,setIsOpen] = useState(false);
@@ -22,44 +23,44 @@ const Sidebar = ({children}) => {
         },
         {
             path:"/about",
-            name:"About",
+            name:"Product",
             icon:<FaUserAlt/>
         },
         {
             path:"/analytics",
-            name:"Analytics",
+            name:"Customers",
             icon:<FaRegChartBar/>
         },
         {
             path:"/comment",
-            name:"Comment",
+            name:"Income",
             icon:<FaCommentAlt/>
         },
         {
             path:"/product",
-            name:"Product",
+            name:"Promote",
             icon:<FaShoppingBag/>
         },
         {
             path:"/productList",
-            name:"Product List",
+            name:"Help",
             icon:<FaThList/>
         }
     ]
     return (
-        <div className="container">
-           <div style={{width: isOpen ? "200px" : "50px"}} className="sidebar">
+        <div className="container-df">
+           <div style={{}} className="sidebar">
                <div className="top_section">
-                   <h1 style={{display: isOpen ? "block" : "none"}} className="logo">Logo</h1>
+               <img src={dashboardImg} alt='dashboard' width='20%' /><h1 style={{ marginLeft:'8px'}} className="logo"> Dashboard</h1>
                    <div style={{marginLeft: isOpen ? "50px" : "0px"}} className="bars">
-                       <FaBars onClick={toggle}/>
+                       {/* <FaBars onClick={toggle}/> */}
                    </div>
                </div>
                {
                    menuItem.map((item, index)=>(
                        <NavLink to={item.path} key={index} className="link" activeclassName="active">
                            <div className="icon">{item.icon}</div>
-                           <div style={{display: isOpen ? "block" : "none"}} className="link_text">{item.name}</div>
+                           <div style={{}} className="link_text">{item.name} </div>
                        </NavLink>
                    ))
                }
